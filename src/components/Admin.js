ab9ece07f078e6241cbdb45da8dccda2ab608e0f
entrpyc <contact@a-angelov.eu>
2021-01-09 17:17:54 +0200

    setup style and structure

---
+++ src/components/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <section className="home">
+      <div className="container">
+        <i>admin</i>
+      </div>
+    </section>
+  )
+}
+
+export default Admin
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <section className="header">
+      <div className="container">
+        header
+      </div>
+    </section>
+  )
+}
+
+export default Header
