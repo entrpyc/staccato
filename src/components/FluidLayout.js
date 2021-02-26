8a735f4c24e9c4b56c8b0b9f672b319c98236585
entrpyc <contact@a-angelov.eu>
2021-02-26 12:05:14 +0200

    update reset

--- src/components/FluidLayout.js
+++ src/components/FluidLayout.js
@@ -5,7 +5,25 @@ import "../styles/modules/_export.scss"
 function FluidLayout() {
   return (
     <section className="home demo-section">
-      <div className="block"></div>
+      <div className="container">
+        <div className="block layout -sidebar">
+          <div className="layout-wrap">
+            <div className="column sidebar d-none-mobile"></div>
+            <div className="column layout -listing fluid">
+              <div className="layout-wrap">
+                <div className="block"></div>  
+                <div className="block"></div>  
+                <div className="block"></div>  
+                <div className="block"></div>  
+                <div className="block"></div>  
+                <div className="block"></div>  
+                <div className="block"></div>  
+                <div className="block"></div>  
+              </div>
+            </div> 
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
