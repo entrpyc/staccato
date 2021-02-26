41a5e6f75cba2e6396b7b842315d850a1b65b982
entrpyc <contact@a-angelov.eu>
2021-02-26 14:12:09 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import FluidLayout from "./components/FluidLayout"
-import FlexBox from "./test/FlexBox"
-import ColGrid from "./test/ColGrid"
-import Header from "./components/Header"
-
-import { ROUTE } from "./helpers/Dictionary"
-
 function App() {
   return (
     <div className="main relative">
-      <Header />
-      <Router>
-          <Switch>
-            <Route exact path={ROUTE.FluidLayout}><FluidLayout /></Route>
-            <Route exact path={ROUTE.TEST.ColGrid}><ColGrid /></Route>
-            <Route exact path={ROUTE.TEST.FlexBox}><FlexBox /></Route>
-          </Switch>
-      </Router>
     </div>
   );
 }
