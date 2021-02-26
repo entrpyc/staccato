f3ac50bd2fdf5ac8a6a46cb5b1a36c7f9e562c78
entrpyc <contact@a-angelov.eu>
2021-02-26 14:28:30 +0200

    add test

---
+++ src/test/ColGrid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import "../styles/modules/ColGrid.scss"
+
+function ColGrid() {
+  return (
+    <section className="home demo-section">
+      <div className="container">
+        <div className="col-grid">
+          <div className="col-3">asdas</div>
+          <div className="col-3">asdas</div>
+          <div className="col-3">asdas</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ColGrid
